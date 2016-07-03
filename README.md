27997166e90c8918e151e2a91467e2e2156ec5fa
lostsnow <lostsnow@gmail.com>
2016-07-03 19:09:31 +0800

    add box publish script

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Requirements
 * Install [VirtualBox][1]
 * Install [Vagrant][2]
 * Install [Packer][3]
-* Download the [CentOS 7 NetInstall ISO][4] and copy/link it into the repo directory 
+* Download the [CentOS 7 NetInstall ISO][4] and copy/link it into the repo directory
 
 Build
 -----
@@ -19,10 +19,27 @@ cd vagrant-centos
 packer build base/centos7.json
 ```
 
+Publish
+-------
+
+```
+# create box version and provider
+make.sh create -v 0.1.0
+# upload box file
+make.sh upload -v 0.1.0 -f build/centos-7-x86_64-virtualbox-base.box
+# release box
+make.sh release -v 0.1.0
+```
+
 Test
 ----
 
 ```shell
+# get box
+vagrant init lostsnow/centos7
+# or
+vagrant box add build/centos-7-x86_64-virtualbox-base.box
+
 vagrant up
 vagrant ssh
 ```
---
+++ make.sh
@@ -0,0 +1,225 @@
+#!/bin/bash
+
+# Abort script at first error
+set -o errexit
+
+# Setting environment variables
+readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+BOX_NAME="lostsnow/centos7"
+BOX_PROVIDER="virtualbox"
+
+# Flag paser
+usage() {
+    case "$1" in
+        create | upload | release )
+            echo -e "Usage: $( basename $0 ) $1 [arg...]"
+            echo
+            echo -e "Options:"
+            echo -e "  -v <version>          box version"
+
+            if [ $1 == "create" ] || [ $1 == "upload" ]; then
+                echo -e "  -p <provider>         box provider, default: virtualbox"
+            fi
+
+            echo -e "  -n <name>             box name, default: lostsnow/centos7"
+
+            if [ $1 == "upload" ]; then
+                echo -e "  -f <file>             box file"
+            fi
+
+            echo
+            exit 1
+            ;;
+        * )
+            echo -e "Usage: $( basename $0 ) <command> [arg...]"
+            echo
+            echo -e "Commands:"
+            echo -e "  help <command>   print command help message"
+            echo -e "  create           create box version and provider"
+            echo -e "  upload           upload box file"
+            echo -e "  release          release box"
+            echo
+            echo -e "Options:"
+            echo -e "  --help           give this help list"
+            echo
+            exit 1
+            ;;
+    esac
+}
+
+catch_all () {
+    [[ ! -z "$1" ]] && warning "$1: command not found"
+    exit 1
+}
+
+function_exists () {
+    type $1 2> /dev/null | grep -q 'function'
+}
+
+cecho () {
+    local MSG=$1
+    local COLOR=$2
+    local PREFIX=
+    local SUFFIX="\e[0m"
+    case "$COLOR" in
+        red)
+            PREFIX="\e[0;31m"
+            ;;
+        green)
+            PREFIX="\e[0;32m"
+            ;;
+        brown)
+            PREFIX="\e[0;33m"
+            ;;
+        purple)
+            PREFIX="\e[0;35m"
+            ;;
+        *)
+            PREFIX=""
+            SUFFIX=""
+        ;;
+    esac
+    MSG="$PREFIX$MSG$SUFFIX"
+    set builtin
+    echo -ne "$MSG"
+}
+ncecho () {
+    cecho "$@"
+    echo
+}
+
+now () {
+    cecho `date "+%Y/%m/%d:%H:%M:%S"` $1;
+}
+
+fatal () {
+    ncecho "[FATAL] $1" "red" && exit 1
+}
+error () {
+    ncecho "[ERROR] $1" red
+}
+success () {
+    ncecho "[SUCCESS] $1" green
+}
+info () {
+    ncecho "[INFO] $1" purple
+}
+
+json_val() {
+    temp=`echo $1 | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $2`
+    echo ${temp##*|}
+}
+
+# Checks for an Atlas API Error
+check_error() {
+    if  [[ $1 == '{"errors":'* ]] ;
+    then
+        error $1
+        exit 1
+    fi
+}
+
+run() {
+    if function_exists _$(basename $0) ; then
+        CMD=$(basename $0)
+    elif function_exists _$1 ; then
+        CMD=$1
+        shift
+    else
+        catch_all $@
+    fi
+
+    while true; do
+        case "$1" in
+            -v )
+                BOX_VERSION="$2"; shift 2 ;;
+            -p )
+                BOX_PROVIDER="$2"; shift 2 ;;
+            -b )
+                BOX_NAME="$2"; shift 2 ;;
+            -f )
+                BOX_FILE="$2"; shift 2 ;;
+            -- )
+                shift; break ;;
+            * )
+                break ;;
+        esac
+    done
+
+    if [ -z "${ATLAS_TOKEN}" ]; then
+        fatal "Atlas token required, Please set by environment variable: ATLAS_TOKEN."
+    fi
+
+    if [ -z "${BOX_VERSION}" ]; then
+        error "Box version required!"
+        usage $CMD
+    fi
+
+    if [ -z "${BOX_NAME}" ]; then
+        error "Box name required!"
+        usage $CMD
+    fi
+
+    _$CMD
+}
+
+_create() {
+    if [ -z "${BOX_PROVIDER}" ]; then
+        error "Box provider required!"
+        usage create
+    fi
+
+    info "create version"
+    response=$(curl -s https://atlas.hashicorp.com/api/v1/box/${BOX_NAME}/versions -X POST -d version[version]=${BOX_VERSION} -d access_token=${ATLAS_TOKEN})
+    check_error $response
+    echo $response
+
+    info "create provider"
+    response=$(curl -s https://atlas.hashicorp.com/api/v1/box/${BOX_NAME}/version/${BOX_VERSION}/providers -X POST -d provider[name]=${BOX_PROVIDER} -d access_token=${ATLAS_TOKEN})
+    check_error $response
+    echo $response
+    success "create success"
+}
+
+_upload() {
+    if [ -z "${BOX_PROVIDER}" ]; then
+        error "Box provider required!"
+        usage upload
+    fi
+
+    if [ -z "${BOX_FILE}" ]; then
+        error "Box file required!"
+        usage upload
+    fi
+
+    if [ ! -f ${BOX_FILE} ]; then
+        error "Box file ${BOX_FILE} not found!"
+        usage upload
+    fi
+
+    info "get upload token"
+    RESPONSE=$(curl -s https://atlas.hashicorp.com/api/v1/box/${BOX_NAME}/version/${BOX_VERSION}/provider/${BOX_PROVIDER}/upload?access_token=${ATLAS_TOKEN})
+    check_error ${RESPONSE}
+    echo ${RESPONSE}
+    TOKEN=$(json_val ${RESPONSE}, "token")
+
+    info "start upload"
+    curl -X PUT --upload-file ${BOX_FILE} https://binstore.hashicorp.com/${TOKEN} --progress-bar > /dev/null
+
+    info "upload success"
+}
+
+_release() {
+    info "release box"
+    response=$(curl -s https://atlas.hashicorp.com/api/v1/box/${BOX_NAME}/version/${BOX_VERSION}/release  -X PUT -d access_token=${ATLAS_TOKEN})
+    check_error $response
+    echo $response
+    success "release success"
+}
+
+case "$1" in
+    -h | --help | help ) usage $2 ;;
+    create | upload | release ) run "$@" ;;
+    * ) error "Invalid command -- $1"; usage ;;
+esac
